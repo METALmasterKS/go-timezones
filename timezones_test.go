2150a53316984c4f16b92d23c1eb129d147c449a
Stanislav Kustov <stanislav.kustov@iqoption.com>
2019-02-22 23:06:18 +0300

    Fix tests

--- timezones_test.go
+++ timezones_test.go
@@ -1,7 +1,6 @@
 package go_timezones
 
 import (
-	"reflect"
 	"testing"
 )
 
@@ -10,13 +9,25 @@ func TestTimezones(t *testing.T) {
 		name string
 		want Timezone
 	}{
+		{"UTC", Timezone("UTC")},
 		{"Europe/Moscow", Timezone("Europe/Moscow")},
+		{"America/North_Dakota/Beulah", Timezone("America/North_Dakota/Beulah")},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := GetTimezones(); !reflect.DeepEqual(got, tt.want) {
+			got := GetTimezones()
+			if !foundedTimezone(tt.want, got) {
 				t.Errorf("Timezones() = %v, want %v", got, tt.want)
 			}
 		})
 	}
+}
+
+func foundedTimezone(found Timezone, sl Timezones) bool {
+	for _, v := range sl {
+		if v == found {
+			return true
+		}
+	}
+	return false
 }
